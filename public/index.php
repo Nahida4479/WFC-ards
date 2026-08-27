b450ade8ea01100467053b99c40e785892e04f37
Nahida <szymon.cyb13@gmail.com>
2026-08-28 00:58:57 +0200

    New CSS style

--- public/index.php
+++ public/index.php
@@ -1,16 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WFC-ards</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
 <?php
 require_once '../config.php';
 $connection = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
-if (!$connection) {
-    die("Connection failed: " . mysqli_connect_error());
-} else {
-    echo "Connected successfully";
-}
+$result = mysqli_query($connection, "SELECT question, answer FROM flashcards ORDER BY RAND() LIMIT 1;");
+
+$row = mysqli_fetch_assoc($result);
+    echo '<div class="flashcard_background"><p class="flashcard_text">' .htmlspecialchars($row['question']) . '</p></div>';
 
 
-$result = mysqli_query($connection, "SELECT * FROM flashcards");
+?>
 
-while ($row = mysqli_fetch_assoc($result)) {
-    echo htmlspecialchars($row['question']) . " - " . htmlspecialchars($row['answer']) . "<br>";
-}
+<div class="flashcard_background" id="flashcard_background">
+    <div class="flashcard_inner">
+        <div class="flashcard_front">
+            <p id="question"><?php echo htmlspecialchars($row['question']); ?> </p>
+        </div>
+            <div class="flashcard_back">
+                <p id="answer"><?php echo htmlspecialchars($row['answer']); ?> </p>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
+
