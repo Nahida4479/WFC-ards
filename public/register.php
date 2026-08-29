a8d0ae3961dbcf535cddd3aa185ba34435cc454b
Nahida <szymon.cyb13@gmail.com>
2026-08-29 21:20:29 +0200

    Add catch error system

--- public/register.php
+++ public/register.php
@@ -14,11 +14,21 @@
 
 <script>
     const password_id = document.getElementById('password_id');
+    const login_id = document.getElementById('login_id')
+    const submit_id = document.getElementById('submit_id');
+    const form = document.querySelector('form')
 
-    if (password_id.length < 8) {
-        alert('Password is too long')
+    document.querySelector('form').addEventListener("submit", (event) => {
+    if (password_id.value.length < 1 || login_id.value.length < 1) {
+        alert("The registration form is incomplete")
+        event.preventDefault();
+    }
+
+    if (password_id.value.length < 8) {
+        alert('Password is too short')
         event.preventDefault();
     } 
+    });
 </script>
 <?php
 require_once '../config.php';
@@ -29,12 +39,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $password = $_POST['password'];
     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-    $result = mysqli_query($connection, "INSERT INTO users (login, password) VALUES ('$login', '$hashed_password')");
-    if (!result) {
-        echo "<script>alert('Error: Username already exists!);</script>";
-    } else {
-        echo "<script>alert('Sucess: Account created!');</script>";
-    }
+    try {
+        $result = mysqli_query($connection, "INSERT INTO users (login, password) VALUES ('$login', '$hashed_password')");
+        echo "<script>alert('Success: Account created!');</script>";
+    } catch (mysqli_sql_exception $e) {
+    echo "<script>alert('Error: Username already exists!');</script>";
+}   
 }
 
 ?>
