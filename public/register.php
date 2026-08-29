73157d285c65278bf8a56362e6551bc61e343f3e
Nahida <szymon.cyb13@gmail.com>
2026-08-29 20:24:14 +0200

    Require password lengt > 8

--- public/register.php
+++ public/register.php
@@ -8,10 +8,18 @@
 <body>
 
 <form method="POST">
-<input type="text" name="login">
-<input type="password" name="password">
+<input type="text" name="login" id="login_id">
+<input type="password" name="password" id="password_id">
 <button type="submit" id="submit_id">Register</button>
 
+<script>
+    const password_id = document.getElementById('password_id');
+
+    if (password_id.length < 8) {
+        alert('Password is too long')
+        event.preventDefault();
+    } 
+</script>
 <?php
 require_once '../config.php';
 $connection = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
@@ -22,6 +30,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
     $result = mysqli_query($connection, "INSERT INTO users (login, password) VALUES ('$login', '$hashed_password')");
+    if (!result) {
+        echo "<script>alert('Error: Username already exists!);</script>";
+    } else {
+        echo "<script>alert('Sucess: Account created!');</script>";
+    }
 }
 
 ?>
